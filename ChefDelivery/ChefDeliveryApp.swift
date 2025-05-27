f7ceb15350715e9500d5d8fe6b747f4f547b31a3
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-05-27 10:55:23 -0300

    Criação do Projeto
    Configurações iniciais do projeto criada.

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Juliano Sgarbossa on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
