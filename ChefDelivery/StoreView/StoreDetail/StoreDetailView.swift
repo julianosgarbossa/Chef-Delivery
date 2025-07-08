9647be12a09b7f4a679513d588b94c8d8c18887e
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-07-08 17:37:23 -0300

    Componentizando StoreDetailView
    Refatorando código para componentizar a tela StoreDetailView, quebrando ela em várias telas menores.

--- ChefDelivery/StoreView/StoreDetail/StoreDetailView.swift
+++ ChefDelivery/StoreView/StoreDetail/StoreDetailView.swift
@@ -11,74 +11,13 @@ struct StoreDetailView: View {
     
     let store: StoreType
     @Environment(\.dismiss) private var dismiss
-    @State private var selectedProduct: ProductType?
     
     var body: some View {
         ScrollView(showsIndicators: false) {
             LazyVStack(alignment: .leading) {
-                Image(store.headerImage)
-                    .resizable()
-                    .scaledToFit()
-                HStack {
-                    Text(store.name)
-                        .font(.title)
-                        .bold()
-                    Spacer()
-                    Image(store.logoImage)
-                }
-                .padding(.vertical, 8)
-                .padding(.horizontal)
-                HStack {
-                    Text(store.location)
-                    Spacer()
-                    ForEach(1...store.stars, id: \.self) { _ in
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.yellow)
-                            .font(.caption)
-                        
-                    }
-                }
-                .padding(.vertical, 8)
-                .padding(.horizontal)
+                StoreDetailHeaderView(store: store)
                 
-                Text("Produtos")
-                    .font(.title2)
-                    .bold()
-                    .padding()
-                
-                ForEach(store.products) { product in
-                    
-                    Button {
-                        selectedProduct = product
-                    } label: {
-                        HStack(spacing: 8) {
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text(product.name)
-                                    .bold()
-                                Text(product.description)
-                                    .foregroundStyle(.black.opacity(0.5))
-                                    .multilineTextAlignment(.leading)
-                                
-                                Text(product.price, format: .currency(code: "BRL"))
-                                    .bold()
-                                
-                            }
-                            Spacer()
-                            Image(product.image)
-                                .resizable()
-                                .scaledToFit()
-                                .cornerRadius(12)
-                                .frame(width: 120, height: 120)
-                                .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
-                            
-                        }
-                        .padding()
-                        .foregroundStyle(.black)
-                    }
-                    .sheet(item: $selectedProduct) { product in
-                        ProductDetailView(product: product)
-                    }
-                }
+                StoreDetailProductsView(products: store.products)
             }
             .navigationTitle(store.name)
             .navigationBarTitleDisplayMode(.inline)
