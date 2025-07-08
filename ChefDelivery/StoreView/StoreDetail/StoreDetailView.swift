4cd0b95bbf85ec09faef4da0a329345cf0711c8e
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-07-08 14:43:50 -0300

    Tela de Detalhes Restaurante
    Criando o layout da tela de detalhes do restaurante com dados vindo do mock.
    
    Criando um botão customizado para voltar para a tela de "Lojas".

--- ChefDelivery/StoreView/StoreDetail/StoreDetailView.swift
+++ ChefDelivery/StoreView/StoreDetail/StoreDetailView.swift
@@ -10,12 +10,85 @@ import SwiftUI
 struct StoreDetailView: View {
     
     let store: StoreType
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        Text(store.name)
+        ScrollView(showsIndicators: false) {
+            LazyVStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
+                HStack {
+                    Text(store.name)
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                    Image(store.logoImage)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                HStack {
+                    Text(store.location)
+                    Spacer()
+                    ForEach(1...store.stars, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.caption)
+                        
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+                ForEach(store.products) { product in
+                    HStack(spacing: 8) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .bold()
+                            Text(product.description)
+                                .foregroundStyle(.black.opacity(0.5))
+                            
+                            Text(product.price, format: .currency(code: "BRL"))
+                                .bold()
+                            
+                        }
+                        Spacer()
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
+                        
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle(store.name)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack(spacing: 4) {
+                            Image(systemName: "cart")
+                            Text("Lojas")
+                        }
+                        .foregroundStyle(Color("ColorRed"))
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    StoreDetailView(store: storesMock[0])
+    StoreDetailView(store: storesMock[2])
 }
